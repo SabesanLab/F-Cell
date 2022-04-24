Major fix for RMS with 0 window sizes. Default rescaling to frame-relative. Updated all default selections for ARVO use (mean-based frame normalization, rms-based summarization).

pyORG_Calculation/ocvl/function/analysis/iORG_profile_analyses.py
@@ -47,7 +47,7 @@ def signal_power_iORG(temporal_profiles, framestamps, summary_method="var", wind
 
         elif window_size < (num_samples/2):
 
-            for i in range(num_samples):
+            for i in range(window_radius, num_samples-window_radius):
 
                 samples = temporal_profiles[:, (i - window_radius):(i + window_radius)]
                 if np.sum(samples[:] != np.nan) > 10:
@@ -65,7 +65,7 @@ def signal_power_iORG(temporal_profiles, framestamps, summary_method="var", wind
 
         elif window_size < (num_samples/2):
 
-            for i in range(num_samples):
+            for i in range(window_radius, num_samples-window_radius):
 
                 samples = temporal_profiles[:, (i - window_radius):(i + window_radius)]
                 if np.sum(samples[:] != np.nan) > 10:
@@ -78,13 +78,16 @@ def signal_power_iORG(temporal_profiles, framestamps, summary_method="var", wind
 
     elif summary_method == "rms":
         if window_radius == 0:
-            iORG = np.nanvar(temporal_profiles, axis=0)
+
+            temporal_profiles **= 2 # Square first
+            iORG = np.nanmean(temporal_profiles, axis=0)  # Average second
+            iORG = np.sqrt(iORG)  # Sqrt last
             num_incl = np.sum(np.isfinite(temporal_profiles), axis=0)
 
         elif window_size < (num_samples/2):
 
             temporal_profiles **= 2 # Square first
-            for i in range(window_radius, num_samples):
+            for i in range(window_radius, num_samples-window_radius):
 
                 samples = temporal_profiles[:, (i - window_radius):(i + window_radius)]
                 if np.sum(samples[:] != np.nan) > 10:

pyORG_Calculation/ocvl/function/iORG_map.py
@@ -84,9 +84,7 @@ if __name__ == "__main__":
         r = 0
         pb["maximum"] = len(allFiles[loc])
         pop_iORG = []
-        pop_iORG_amp = np.empty((len(allFiles[loc])-skipnum+1))
-        pop_iORG_amp[:] = np.nan
-        pop_iORG_num = []
+        pop_iORG_num=[]
         framestamps = []
         max_frmstamp = 0
         plt.figure(0)
@@ -113,7 +111,7 @@ if __name__ == "__main__":
                     perm = np.arange(len(dataset.coord_data))
                 print("Analyzing " + str(len(perm)) + " cells.")
 
-                temp_profiles = extract_profiles(dataset.video_data, dataset.coord_data[perm, :], seg_radius=1)
+                temp_profiles = extract_profiles(dataset.video_data, dataset.coord_data[perm, :], seg_radius=2)
                 norm_temporal_profiles = norm_profiles(temp_profiles, norm_method="mean")
                 stdize_profiles = standardize_profiles(norm_temporal_profiles, dataset.framestamps,
                                                        dataset.stimtrain_frame_stamps[0], method="mean_sub")
@@ -121,24 +119,9 @@ if __name__ == "__main__":
                 #plt.savefig(os.path.join(res_dir, file[0:-4] + "_all_std_profiles.svg"))
 
                 framestamps.append(dataset.framestamps)
-                tmp_iorg, tmp_incl = signal_power_iORG(stdize_profiles, dataset.framestamps, summary_method="rms", window_size=1)
-
-                prestim_amp = np.nanmedian(tmp_iorg[0:dataset.stimtrain_frame_stamps[0]])
-                poststim = tmp_iorg[dataset.stimtrain_frame_stamps[1]:(dataset.stimtrain_frame_stamps[1] + 15)]
-                if poststim.size == 0:
-                    poststim_amp = 0
-                    prestim_amp = 0
-                else:
-                    poststim_amp = np.amax(poststim)
-
-                    pop_iORG.append(tmp_iorg)
-                    pop_iORG_num.append(tmp_incl)
-
-                pop_iORG_amp[r] = (poststim_amp - prestim_amp)
-
-                print("iORG Simple Amplitude: " + str(pop_iORG_amp[r]) + " (prestim: "+str(prestim_amp) +
-                                                                          " poststim: " + str(poststim_amp) +")")
-
+                tmp_iorg, tmp_incl = signal_power_iORG(stdize_profiles, dataset.framestamps, summary_method="var", window_size=1)
+                pop_iORG.append(tmp_iorg)
+                pop_iORG_num.append(tmp_incl)
                 plt.figure(0)
                 plt.plot(dataset.framestamps, pop_iORG[r-skipnum], color=mapper.to_rgba(r-skipnum, norm=False), label=file.name)
                 plt.show(block=False)
@@ -148,10 +131,9 @@ if __name__ == "__main__":
 
             r += 1
         #plt.legend()
-        plt.savefig(res_dir.joinpath(this_dirname + "_pop_iORG.svg"))
         plt.savefig( res_dir.joinpath(this_dirname + "_pop_iORG.png") )
 
-
+        del dataset
         # Grab all of the
         all_iORG = np.empty((len(pop_iORG), max_frmstamp+1))
         all_iORG[:] = np.nan
@@ -162,31 +144,16 @@ if __name__ == "__main__":
             all_iORG[i, framestamps[i]] = iorg
 
         # Pooled variance calc
-        pooled_iORG = np.nansum( all_incl*all_iORG, axis=0 ) / np.nansum(all_incl, axis=0)
-        #pooled_stddev_iORG = np.sqrt(pooled_var_iORG)
-
-        prestim_amp = np.nanmedian(pooled_iORG[0:dataset.stimtrain_frame_stamps[0]])
-        poststim = pooled_iORG[dataset.stimtrain_frame_stamps[1]:(dataset.stimtrain_frame_stamps[1] + 15)]
-        if poststim.size == 0:
-            poststim_amp = 0
-        else:
-            poststim_amp = np.amax(poststim)
-
-        pop_iORG_amp[r] = (poststim_amp - prestim_amp)
-
-        print("iORG Avg Amplitude: " + str(pop_iORG_amp[r]) + " (prestim: " + str(prestim_amp) +
-              " poststim: " + str(poststim_amp) + ")")
+        pooled_var_iORG = np.nansum( (all_incl-1)*all_iORG, axis=0 ) / np.nansum(all_incl-1, axis=0)
+        pooled_stddev_iORG = np.sqrt(pooled_var_iORG)
 
         pop_dFrame = pd.DataFrame(np.concatenate((all_iORG,
-                                                  np.reshape(pooled_iORG, (1, len(pooled_iORG)))), axis=0))
+                                                  np.reshape(pooled_stddev_iORG, (1, len(pooled_stddev_iORG)))), axis=0))
         pop_dFrame.to_csv(res_dir.joinpath(this_dirname + "_pop_iORG.csv"), header=False)
-
-        pop_amp_dFrame = pd.DataFrame(pop_iORG_amp)
-        pop_amp_dFrame.to_csv(res_dir.joinpath(this_dirname + "_pop_iORG_amp.csv"), header=False)
 
         plt.figure(1)
         plt.clf()
-        plt.plot(pooled_iORG)
+        plt.plot(pooled_stddev_iORG)
         plt.show(block=False)
         plt.savefig(res_dir.joinpath(this_dirname + "_pooled_pop_iORG.png"))
         plt.savefig(res_dir.joinpath(this_dirname + "_pooled_pop_iORG.svg"))

pyORG_Calculation/ocvl/function/individual_cell_iORG.py
@@ -73,7 +73,11 @@ if __name__ == "__main__":
     root.geometry('%dx%d+%d+%d' % (w, h, x, y))
     root.update()
 
-    for loc in allFiles:
+    first = True
+    cell_framestamps = []
+    cell_profiles = []
+
+    for l, loc in enumerate(allFiles):
         res_dir = loc.joinpath("Results")
         res_dir.mkdir(exist_ok=True)
 
@@ -83,9 +87,8 @@ if __name__ == "__main__":
         pb["maximum"] = len(allFiles[loc])
         mapper = plt.cm.ScalarMappable(cmap=plt.get_cmap("viridis", len(allFiles[loc])).reversed())
         max_frmstamp = 0
-        cell_framestamps = []
-        cell_profiles = []
-        first = True
+
+
         for file in allFiles[loc]:
 
             if "ALL_ACQ_AVG" not in file.name:
@@ -107,6 +110,9 @@ if __name__ == "__main__":
                         framerate = dataset.framerate
                         stimulus_train = dataset.stimtrain_frame_stamps
 
+                        simple_amp = np.empty((len(allFiles), len(coord_data)))
+                        simple_amp[:] = np.nan
+
                     first = False
 
                 temp_profiles = extract_profiles(dataset.video_data, dataset.coord_data, seg_radius=1)
@@ -117,7 +123,7 @@ if __name__ == "__main__":
 
                 # Put the profile of each cell into its own array
                 for c in range(len(dataset.coord_data)):
-                    cell_framestamps[c].append(dataset.framestamps)
+                    cell_framestamps[c].append(dataset.framestamps) # HERE IS THE PROBLEM, YO
                     cell_profiles[c].append(stdize_profiles[c, :])
                 r += 1
 
@@ -140,18 +146,15 @@ if __name__ == "__main__":
             for i, profile in enumerate(cell_profiles[c]):
                 all_cell_iORG[i, cell_framestamps[c][i], c] = profile
 
-        print("Yay!")
 
-        simple_amp = np.empty((len(coord_data)))
-        simple_amp[:] = np.nan
         for c in range(len(coord_data)):
             cell_power_iORG[c, :], numincl = signal_power_iORG(all_cell_iORG[:, :, c], full_framestamp_range,
-                                                               summary_method="std", window_size=1)
+                                                               summary_method="rms", window_size=1)
 
             prestim_amp = np.nanmedian(cell_power_iORG[c, 0:stimulus_train[0]])
             poststim_amp = np.nanmedian(cell_power_iORG[c, stimulus_train[1]:(stimulus_train[1]+10)])
 
-            simple_amp[c] = poststim_amp-prestim_amp
+            simple_amp[l, c] = poststim_amp-prestim_amp
 
             # plt.figure(0)
             # plt.clf()
@@ -165,8 +168,8 @@ if __name__ == "__main__":
            # plt.savefig(res_dir.joinpath(this_dirname +  + "_allcell_iORG_amp.png"))
 
         plt.figure(1)
-        histbins = np.arange(start=-10, stop=100, step=2.5)
-        plt.hist(simple_amp, bins=histbins)
+        histbins = np.arange(start=-0.2, stop=1.5, step=0.025)
+        plt.hist(simple_amp[l, :], bins=histbins)
         # plt.plot(cell_power_iORG[c, :], "k-", alpha=0.05)
         plt.show(block=False)
         plt.savefig(res_dir.joinpath(this_dirname + "_allcell_iORG_amp.png"))
@@ -184,7 +187,7 @@ if __name__ == "__main__":
         for c, cell in enumerate(vor.regions[1:]):
             if not -1 in cell:
                 poly = [vor.vertices[i] for i in cell]
-                plt.fill(*zip(*poly), color=hist_mapper.to_rgba(simple_amp[c]))
+                plt.fill(*zip(*poly), color=hist_mapper.to_rgba(simple_amp[l, c]))
         ax = plt.gca()
         ax.set_aspect("equal", adjustable="box")
         plt.show(block=False)