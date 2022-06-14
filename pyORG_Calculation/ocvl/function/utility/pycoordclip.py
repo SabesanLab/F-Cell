2178d9f190554486a4e2fccd400bd6c60ee37317
mgaffney-mcw <mgaffney@mcw.edu>
2022-06-14 13:14:17 -0500

    Defining pycoordclip further, calling in individual_cell_iORG for testing, moving pycoordclip function to utility folder

--- pyORG_Calculation/ocvl/function/utility/pycoordclip.py
+++ pyORG_Calculation/ocvl/function/utility/pycoordclip.py
@@ -10,7 +10,7 @@ from ocvl.function.utility.meao import MEAODataset
 from ocvl.function.utility.temporal_signal_utils import reconstruct_profiles
 
 
-def pycoordclip(image_stack, coordinates=None, seg_mask="box", seg_radius=1, summary="mean", centroid=None, display=False):
+def pycoordclip(coordinates, thresholdr, thresholdc, inoutorxor='i'):
     """
     Original matlab fxn by Robert Cooper
     Adaptation by Mina Gaffney
@@ -19,7 +19,6 @@ def pycoordclip(image_stack, coordinates=None, seg_mask="box", seg_radius=1, sum
     This function removes all coordinates less than or greater than a specific threshold, in an n-defined polygon around
     the image. A python version of the original matlab fxn coordclip() by Robert Cooper, 2011.
 
-    :param image_stack: a YxXxZ numpy matrix, where there are Y rows, X columns, and Z samples.
     :param coordinates: input as X/Y, these mark locations the locations that will be extracted from all S samples.
     :param thresholdr: pixel row coordinates that are allowed to remain in the list
     :param thresholdc: pixel col coordinates that are allowed to remain in the list
@@ -30,3 +29,9 @@ def pycoordclip(image_stack, coordinates=None, seg_mask="box", seg_radius=1, sum
     :return: a list of x,y coordinates to be included for further analysis based on an input threshold
     """
 
+    # Check that coordinates are present
+    if coordinates == []:
+        raise Exception("No coordinates were given! Can't run pycoordclip.")
+
+    # Determine image size based on coordinates
+    imsizecol = np.max(coordinates)
