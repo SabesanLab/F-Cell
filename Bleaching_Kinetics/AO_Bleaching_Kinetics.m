7aa0a2b9a184e7ad7804e52f842ebb8d1b8726c6
Robert F Cooper <robertfcoop@gmail.com>
2023-01-13 10:03:09 -0600

    Added scotopic vs photopic bleaching curves.

--- Bleaching_Kinetics/AO_Bleaching_Kinetics.m
+++ Bleaching_Kinetics/AO_Bleaching_Kinetics.m
@@ -8,9 +8,10 @@ clear;
 % close all force;
 % tbDeployToolboxes('registered',{'isetbio', 'BrainardLabToolbox','Psychtoolbox-3','SilentSubstitutionToolbox'})
 
+type = "scotopic"
 units = 'trolands';
-stim_lambda = 554; % in nm
-stim_irradiance =  3.78; % in uW
+stim_lambda = 514; % in nm
+stim_irradiance =  3.6; % in uW
 
 num_acquisitions = 10;
 single_trial_train = [0 1  0;
@@ -35,13 +36,28 @@ end
 
 
 
-[~,I] = AOLightLevelConversions_Func(1, stim_lambda, stim_irradiance, true); % Stimulus Intensity in Td
+[~,I_cones, I_rods] = AOLightLevelConversions_Func(1, stim_lambda, stim_irradiance, true); % Stimulus Intensity in Td
+
+if strcmp(type, "photopic")
+    I = I_cones;
+elseif strcmp(type, "scotopic")
+    I = I_rods;
+end
 
 switch (units)
     case 'trolands'
-        I_0 = 20000; % in Td %Stimulus intensity that bleaches at the rate of 1/N
-                      % 73.7 Td for rods, 20000 Td for cones
-        N = 120; % Scaling factor, where 400=rhodopsin, 120=L/M cones.
+        if strcmp(type, "photopic")
+            I_0 = 20000; % in Td %Stimulus intensity that bleaches at the rate of 1/N
+                          % 19,952 Td for rods, 20000 Td for cones
+        
+            N = 120; % Scaling factor, where 400=rhodopsin, 120=L/M cones.
+        elseif strcmp(type, "scotopic")
+            I_0 = 19952; % in Td %Stimulus intensity that bleaches at the rate of 1/N
+                      % 19,952 Td for rods, 20000 Td for cones
+        
+            N = 400; % Scaling factor, where 400=rhodopsin, 120=L/M cones.
+        end
+
     case 'isomerizations'
         I_0 = 10^6.4;
         N = 120; % Scaling factor
