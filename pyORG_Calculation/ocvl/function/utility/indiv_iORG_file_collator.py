2d4c6a8aaece20ff7fe805ad462af16d3f5cbcce
Robert F Cooper <robertfcoop@gmail.com>
2024-01-14 13:43:59 -0600

    Changed filed output to be more robust and useful for imperfect eccentricities relative to a normative dataset.

--- pyORG_Calculation/ocvl/function/utility/indiv_iORG_file_collator.py
+++ pyORG_Calculation/ocvl/function/utility/indiv_iORG_file_collator.py
@@ -7,6 +7,9 @@ from matplotlib import pyplot as plt
 
 if __name__ == "__main__":
 
+
+
+
     metric_header = "Amplitude"
 
     root = Tk()
@@ -31,6 +34,22 @@ if __name__ == "__main__":
     totFiles = 0
     # Parse out the locations and filenames, store them in a hash table.
     searchpath = Path(pName)
+
+
+    # For looking at the Xth percentile on a per-subject basis compared to our normative baseline,
+    # as well as determining where data should be grouped for IRDs
+    normie_path = searchpath.joinpath("Normal_collated_Amplitude_meandata.csv")
+
+    if normie_path.is_file():
+
+        perc_cutoff = 0.05
+        normie_table = pd.read_csv(normie_path, index_col=0).sort_index()
+        normie_dat = normie_table.to_numpy()
+        above_cut = np.argmax(normie_dat > perc_cutoff, axis=1, keepdims=True)
+
+        normie_locs = normie_table.index.to_numpy().astype(float)
+        normie_vals = normie_table.columns.to_numpy()[above_cut.flatten()].astype(float)
+
     for path in searchpath.rglob("*.csv"):
         if "log_amplitude_cumhist" in path.name:
             splitfName = path.name.split("_")
@@ -49,29 +68,41 @@ if __name__ == "__main__":
 
     all_data = pd.DataFrame()
     all_means = pd.DataFrame()
-
+    subID = dict()
+    all_cumhist = dict()
     # NEVER grow a dataframe rowwise, or baby Jesus will cry
     # https://stackoverflow.com/questions/13784192/creating-an-empty-pandas-dataframe-and-then-filling-it
     for locid in allFiles:
 
-        all_cumhist = np.full((len(allFiles[locid]), 110), np.nan)
+        if normie_path.is_file():
+            normie_loc_ind = np.argmin(np.round(abs(abs(float(locid)) - normie_locs)))
+            thisloc = normie_locs[normie_loc_ind]
+        else:
+            thisloc = locid
+
+        if str(thisloc) not in subID:
+            subID[str(thisloc)] = []
+            all_cumhist[str(thisloc)] = np.full((len(allFiles[locid]), 550), np.nan)
+        else:
+            all_cumhist[str(thisloc)] = np.append(all_cumhist[str(thisloc)], np.full((len(allFiles[locid]), 550), np.nan), axis=0)
+        print(thisloc)
         # The above may change depending on how many bins we go with.
         f = 0
-        subID = []
 
-        plt.figure(0)
-        plt.clf()
+
+        plt.figure(str(thisloc*10))
+
         for locfile in allFiles[locid]:
 
             stat_table = pd.read_csv(locfile, delimiter=",", header=0, encoding="utf-8-sig")
             bins = stat_table.loc[1, :]
 
-            subID.append(locfile.parent.name)
+            subID[str(thisloc)].append(locfile.parent.name)
 
             cumhist = np.nancumsum(stat_table.loc[0, :])
             cumhist /= np.amax(cumhist.flatten()) # Normalize to 1 as its a probability density, not a probability mass, function
 
-            all_cumhist[f, :] = cumhist
+            all_cumhist[str(thisloc)][f, :] = cumhist
             f+=1
 
             plt.plot(bins, cumhist)
@@ -79,29 +110,28 @@ if __name__ == "__main__":
         # all_cumhist=np.nancumsum(all_cumhist, axis=1)
         # all_cumhist /= np.amax(all_cumhist.flatten())
         plt.show(block=False)
-        plt.legend(subID)
-        plt.savefig(resultdir.joinpath(resultdir.name + "_" + locid + "_all_cumulative_histograms.svg"))
+        plt.legend(subID[str(thisloc)])
+        plt.savefig(resultdir.joinpath(resultdir.name + "_" + str(thisloc) + "_all_cumulative_histograms.svg"))
 
-        mean_cumhist = np.mean(all_cumhist, axis=0)
-        stddev_cumhist = np.std(all_cumhist, axis=0)
+        mean_cumhist = np.mean(all_cumhist[str(thisloc)], axis=0)
+        stddev_cumhist = np.std(all_cumhist[str(thisloc)], axis=0)
         conf_interval = 2 * stddev_cumhist / np.sqrt(len(allFiles[locid]))
 
-        plt.figure(locid)
+        plt.figure(str(thisloc*10+100))
         plt.fill_between(bins, mean_cumhist + conf_interval, color=[0, 0, 0, 0.3])
         plt.fill_between(bins, mean_cumhist - conf_interval, color=[1, 1, 1, 1])
         plt.plot(bins, mean_cumhist)
-        plt.title(locid)
-        plt.savefig(resultdir.joinpath(resultdir.name+ "_"+locid+"_mean_n_conf_cumulative_histogram.svg"))
+        plt.savefig(resultdir.joinpath(resultdir.name+ "_"+str(thisloc)+"_mean_n_conf_cumulative_histogram.svg"))
 
         plt.show(block=False)
         #plt.waitforbuttonpress()
 
-        indices = pd.MultiIndex.from_product([[locid], subID],
+        indices = pd.MultiIndex.from_product([[thisloc], subID[str(thisloc)]],
                                              names=["Location", "ID"])
-        subframe = pd.DataFrame(all_cumhist, index=indices)
+        subframe = pd.DataFrame(all_cumhist[str(thisloc)], index=indices)
 
         all_data=pd.concat([all_data, subframe])
-        all_means=pd.concat([all_means, pd.DataFrame(mean_cumhist[np.newaxis, :], index=[locid], columns=bins)])
+        all_means=pd.concat([all_means, pd.DataFrame(mean_cumhist[np.newaxis, :], index=[thisloc], columns=bins)])
         print("wut")
 
 
@@ -110,19 +140,8 @@ if __name__ == "__main__":
     all_means.to_csv(resultdir.joinpath(resultdir.name + "_collated_" + metric_header + "_meandata.csv"))
 
 
-
-    # Do the same as above, but expressly looking at the Xth percentile on a per-subject basis compared to our normative baseline.
-    normie_path = searchpath.joinpath("Normal_collated_Amplitude_meandata.csv")
     if normie_path.is_file():
-
-        perc_cutoff = 0.05
-
-        normie_table = pd.read_csv(normie_path, index_col=0).sort_index()
-        normie_dat = normie_table.to_numpy()
-        above_cut = np.argmax(normie_dat > perc_cutoff, axis=1, keepdims=True)
-
-        normie_locs = normie_table.index.to_numpy().astype(float)
-        normie_vals = normie_table.columns.to_numpy()[above_cut.flatten()].astype(float)
+        perc_data = pd.DataFrame()
 
         allSubFiles = dict()
         totSubFiles = 0
@@ -146,7 +165,7 @@ if __name__ == "__main__":
             fiftperc = []
             loc = []
             subID.append(subid.name)
-
+            print(subid.name)
             f = 0
             for file in allSubFiles[subid]:
                 splitfName = file.name.split("_")
@@ -162,16 +181,29 @@ if __name__ == "__main__":
                 cumhist /= np.amax(cumhist.flatten())
 
                 # Find the percentage of cones in this subject that are within the normative percentage we set above.
-                perc_healthy = 100.0*(1-cumhist[np.flatnonzero(bins >= normie_vals[normie_loc_ind])[0]]+0.05)
+                perc_healthy = 100.0*(1-cumhist[np.flatnonzero(bins >= normie_vals[normie_loc_ind])[0]])
+                print(cumhist[np.flatnonzero(bins >= normie_vals[normie_loc_ind])[0]])
 
                 fiftperc.append(perc_healthy)
                 loc.append(firstcoord)
+
+
                 f+=1
+
+            indices = pd.MultiIndex.from_product([loc, [subid.name]],
+                                                 names=["Location", "ID"])
+            subframe = pd.DataFrame(fiftperc, index=indices)
+
+            perc_data = pd.concat([perc_data, subframe])
+
 
             plt.plot(loc, fiftperc,".-")
             plt.show(block=False)
+            plt.legend(subID)
             plt.savefig(resultdir.joinpath(resultdir.name + "_percent_overnormie.svg"))
-        plt.legend(subID)
+
+        perc_data.to_csv(resultdir.joinpath(resultdir.name + "_percent_overnormie_data.csv"))
+
 
 
 plt.waitforbuttonpress()
